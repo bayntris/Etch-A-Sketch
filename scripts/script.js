a8ef6ad566d414c2be7374e679187a962f6cc289
bayntris <tristanragtdotl@gmail.com>
2023-06-08 13:10:03 +1000

    Add Hover Effect

--- scripts/script.js
+++ scripts/script.js
@@ -9,18 +9,33 @@ grid.classList.add('grid');
 console.log(container);
 
 for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    div.classList.add('div');
-    div.style.display = 'flex';
-    
+    //Create a Div Row with display of Flex
+    const divRow = document.createElement('div');
+    divRow.classList.add('row');
+    divRow.style.display = 'flex';
    
-    grid.appendChild(div);
+    
+   // Add the Div Row to the grid
+    grid.appendChild(divRow);
 
+    // Add 16 Square Divs to each Div Row
     for (let j = 0; j < 16; j++) {
         const divSquare = document.createElement('div');
         divSquare.classList.add('divSquare');
-        div.appendChild(divSquare);
-    }
 
+        // Applying 'Hover' effect on each Square Div
+
+        divSquare.addEventListener('mouseover', (e) => {
+            // Add 'changeYellow' class to each Div Square
+            divSquare.classList.add('changeYellow');
+            
+        })
+
+
+
+
+        divRow.appendChild(divSquare);
+    } 
+    // Append the grid to the Container div
     container.appendChild(grid);
 }

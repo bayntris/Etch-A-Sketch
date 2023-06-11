c8e40e6d6b82b8494d9371afa055ce1048128cc2
bayntris <tristanragtdotl@gmail.com>
2023-06-11 11:35:56 +1000

    Make Grid Cells sizing dynamic

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,24 @@ const grid = document.querySelector('#grid');
 
 
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 30; i++) {
 
     // Create a Row
     const gridRow = document.createElement('div');
     gridRow.style.display = 'flex';
 
     // In each Row, create 16 Grid Cells
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < 30; j++) {
         const gridCell = document.createElement('div');
-        gridCell.style.height = '20px';
-        gridCell.style.width = '20px';
+        const dynamicWidth = 600 / 30;
+        const dynamicHeight = 600 / 30;
+
+        gridCell.style.width = `${dynamicWidth}px`;
+        gridCell.style.height = `${dynamicHeight}px`;
+        
+        //gridCell.style.flex = '1';
+       // gridCell.style.height = '20px';
+       // gridCell.style.width = '20px';
         gridCell.style.border = '1px solid black';
 
         gridRow.appendChild(gridCell);
